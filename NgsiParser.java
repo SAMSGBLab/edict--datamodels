aeddb868009420e50bef36c6e0d75981acbcad62
mahditrabolsi <mahditrabolsi@gmail.com>
2023-04-11 11:05:59 +0200

    Updated the parser

---
+++ NgsiParser.java
@@ -0,0 +1,62 @@
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import iotSystemComponents.Application;
+import iotSystemComponents.ApplicationCategory;
+import iotSystemComponents.IoTdevice;
+import iotSystemComponents.Topic;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class NgsiParser {
+
+    public static HashMap<String,ApplicationCategory> applicationCategories = new HashMap<>();
+    public static HashMap<String,Application> applications = new HashMap<>();
+    public static HashMap<String,Topic> observations = new HashMap<>();
+    public static HashMap<String,IoTdevice> devices = new  HashMap<>();
+    public static <T> T NGSI_parse(File file, Class<T> cl) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return mapper.readValue(file, cl);
+
+    }
+
+    public static void main(String[] args) {
+
+        String folder = "C:\\Users\\HP\\Documents\\NGSI\\edict--datamodels\\medium-load";
+        File[] folders = new File(folder).listFiles();
+
+        for (File model : folders) {
+            File[] files = model.listFiles();
+            int fileCount = files.length;
+            System.out.println(model + " : " + fileCount);
+
+            for (File file : files) {
+                String id = file.getName().substring(0, file.getName().lastIndexOf('.'));
+                try {
+                    switch (model.getName()) {
+                        case "applicationCategories":
+                            //key = filename without extension
+                            applicationCategories.put( id, NGSI_parse(file, ApplicationCategory.class));
+                            break;
+                        case "applications":
+                            applications.put( id, NGSI_parse(file, Application.class));
+                            break;
+                        case "devices":
+                            devices.put( id, NGSI_parse(file, IoTdevice.class));
+                            break;
+                        case "observations":
+                            observations.put( id, NGSI_parse(file, Topic.class));
+                            break;
+                        default:
+                            System.out.println("Unknown model: " + model.getName());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+}
--- iotSystemComponents/ApplicationCategory.java
+++ iotSystemComponents/ApplicationCategory.java
@@ -1,13 +1,27 @@
 package iotSystemComponents;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import java.util.Map;
+
 public class ApplicationCategory {
 
+	@JsonProperty("id")
 	public String categoryId;
+	@JsonProperty("name")
 	public String categoryName;
 	public double processingTime;
-	
+	public ApplicationCategory() {
+	}
 	public ApplicationCategory(String categoryId, String categoryName) {
 		this.categoryId = categoryId;
 		this.categoryName = categoryName;
+	}
+	@JsonProperty("name")
+	private void unpackName(Map<String, Object> nameMap) {
+		if (nameMap.containsKey("value")) {
+			categoryName = (String) nameMap.get("value");
+		}
 	}
 }
